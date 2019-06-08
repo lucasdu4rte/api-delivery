e2594d824f185863ccb4cc1ea58a1280f3d48070
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-08 11:28:14 -0300

    sessions return user

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,17 @@
 "use strict";
+const User = use("App/Models/User");
 
 class SessionController {
-  async store({ request, auth }) {
+  async store({ request, response, auth }) {
     const { email, password } = request.all();
+    try {
+      const token = await auth.attempt(email, password)
+      const user = await User.query().where('email', email).first()
 
-    const token = await auth.attempt(email, password);
-
-    return token;
+      return {...token, user};
+    } catch (error) {
+      return response.status(404).json({ error: error.message })
+    }
   }
 }
 
