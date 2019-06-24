e40b54acf42c940732b9c05e48db90d3e6de8711
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-23 23:54:47 -0300

    Add methods in order controller and removed unsed

--- app/Controllers/Http/OrderController.js
+++ app/Controllers/Http/OrderController.js
@@ -1,92 +1,50 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Order = use("App/Models/Order");
 
-/**
- * Resourceful controller for interacting with orders
- */
 class OrderController {
-  /**
-   * Show a list of all orders.
-   * GET orders
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async index ({ request, response, view }) {
+    // const { type_id } = request.only(["type_id"]);
+    const query = Order.query()
+    // if (type_id) {
+    //   query.types().whereInPivot("type_id", type_id)
+    // }
+
+    const orders = await query.fetch();
+
+    return orders;
   }
 
-  /**
-   * Render a form to be used for creating a new order.
-   * GET orders/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new order.
-   * POST orders
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async store ({ request, response }) {
+    const { ...data } = request.all()
+
+    const order = await Order.create(data)
+
+    return order
   }
 
-  /**
-   * Display a single order.
-   * GET orders/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async show ({ params, request, response, view }) {
+    const order = await Order.findOrFail(params.id);
+
+    return order;
   }
 
-  /**
-   * Render a form to update an existing order.
-   * GET orders/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update order details.
-   * PUT or PATCH orders/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async update ({ params, request, response }) {
+    const order = await Order.findOrFail(params.id);
+
+    const data = request.only(["description", "price", "photo_url", "type_id"]);
+
+    order.merge(data);
+
+    await order.save();
+
+    return order;
   }
 
-  /**
-   * Delete a order with id.
-   * DELETE orders/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
   async destroy ({ params, request, response }) {
+    const order = await Order.findOrFail(params.id);
+
+    await order.delete();
   }
 }
 
